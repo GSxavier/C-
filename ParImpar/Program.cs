bd7a75cb394d2938e6b16aed26314ecefd58f9eb
Gustavo Xavier <mineirinhotico02@gmail.com>
2019-09-20 17:27:40 -0300

    Exercicio

---
+++ ParImpar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+
+          while(num !=0){
+                 Console.WriteLine("Digite um numero:");
+                  num = int.Parse(Console.ReadLine());
+
+            if( num % 2 == 0){
+                Console.WriteLine("Numero impar");
+            }
+
+            else {
+              Console.WriteLine("Numero par");
+                
+            }
+             
+          }        
+        }
+    }
+}
